571f9bc28d639aae5f8637231e37504111b727ee
Wesley Hales <wesleyhales@gmail.com>
2011-03-28 15:07:41 -0400

    typical structural changes for JSF2 project, working search on home page

---
+++ src/main/java/org/richfaces/examples/twitterclient/TwitterClient.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.examples.twitterclient;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Tweet;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+import javax.inject.Named;
+import java.util.List;
+
+/** @author <a href="mailto:whales@redhat.com">Wesley Hales</a> */
+@Named
+public class TwitterClient
+{
+
+   public List getTweets(String searchTerm){
+      Twitter twitter = new TwitterFactory().getInstance();
+      List<Tweet> tweets = null;
+        try {
+            QueryResult result = twitter.search(new Query(searchTerm));
+            tweets = result.getTweets();
+            for (Tweet tweet : tweets) {
+                System.out.println("@" + tweet.getFromUser() + " - " + tweet.getText());
+            }
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+        }
+      return tweets;
+   }
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,1 +1,1 @@
-<% response.sendRedirect("home.jsf"); %>
+<% response.sendRedirect("pages/home.jsf"); %>
