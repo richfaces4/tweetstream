2451ea9d7c95382c11304a36d711eb165584fe4f
Wesley Hales <wesleyhales@gmail.com>
2011-07-27 10:04:01 -0400

    oops forgot to add new cache builder

---
+++ tweetstream/src/main/java/org/richfaces/examples/tweetstream/dataserver/cache/SimpleCacheBuilder.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.examples.tweetstream.dataserver.cache;
+
+import org.infinispan.manager.EmbeddedCacheManager;
+
+import javax.enterprise.inject.Alternative;
+import javax.inject.Singleton;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:whales@redhat.com">Wesley Hales</a>
+ */
+
+@Singleton
+@Alternative
+public class SimpleCacheBuilder implements CacheBuilder, Serializable {
+
+    private Map cache;
+
+    @Override
+    public <K, V> Map<K, V> getCache() {
+        if (cache == null){
+            cache = new HashMap<K, V>();
+        }
+        return cache;
+    }
+
+    @Override
+    public <K, V> Map<K, V> getCache(String cacheName) {
+        return cache;
+    }
+
+    @Override
+    public EmbeddedCacheManager getCacheContainer() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
